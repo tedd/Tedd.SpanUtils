0c28e2ecaab2d165c580eefa1c1b63107f135b2a
Tedd Hansen <tedd@konge.net>
2019-12-23 21:10:37 +0100

    ToInt32() and ToUInt32()

--- src/Tedd.SpanUtils/UInt24Extensions.cs
+++ src/Tedd.SpanUtils/UInt24Extensions.cs
@@ -14,12 +14,30 @@ namespace Tedd.SpanUtils
         public static UInt24 ToUInt24(this UInt32 value) => (UInt24)(value & 0xFFFFFF);
 
         /// <summary>
+        /// Convert Int32 to UInt24, will perform & 0xFFFFF to truncate Int32.
+        /// WARNING: Negative values will be lost, and are not restored with .ToInt32().
+        /// </summary>
+        /// <remarks>Negative values will be lost, and are not restored with .ToInt32().</remarks>
+        /// <param name="value">Value to convert.</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UInt24 ToUInt24(this Int32 value) => (UInt24)(value & 0xFFFFFF);
+        /// <summary>
         /// Converts UInt24 to UInt32.
         /// </summary>
         /// <param name="value">Value to convert.</param>
         /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static UInt32 ToInt32(this UInt24 value) => (UInt32)value;
+        public static UInt32 ToUInt32(this UInt24 value) => (UInt32)value;
+
+        /// <summary>
+        /// Converts UInt24 to Int32.
+        /// </summary>
+        /// <param name="value">Value to convert.</param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Int32 ToInt32(this UInt24 value) => (Int32)value;
+
 
     }
 }
