8a178a525094755b9c223fce9bf960210cc110d7
Tedd Hansen <tedd@konge.net>
2020-01-03 17:49:23 +0100

    Cleanup

--- src/Tedd.SpanUtils/UInt32Extensions.cs
+++ src/Tedd.SpanUtils/UInt32Extensions.cs
@@ -8,7 +8,6 @@ namespace Tedd.SpanUtils
         /// <summary>
         /// Counts how many bytes WriteSize will use for a given value.
         /// </summary>
-        /// <param name="span"></param>
         /// <param name="value"></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int MeasureWriteSize(this UInt32 value) => Utils.MeasureWriteSize(value);
