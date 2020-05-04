e2a85f0bb212e0624894308b0ddaa40f088cfbdf
Tedd Hansen <tedd@konge.net>
2020-05-04 13:32:32 +0200

    [gh-3] Benchmarks added for int and long. Results consistent with decompiled code. I.e. with 64-bit integer if last byte is written first there is fewer instructions because it avoids 7 if-branches, and this gives 6% speed increase.

---
+++ src/Tedd.SpanUtils.Benchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Running;
+using Tedd.SpanUtils.Benchmark.Tests;
+
+namespace Tedd.SpanUtils.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary1 = BenchmarkRunner.Run<SpanCopyMethodInt>();
+            var summary2 = BenchmarkRunner.Run<SpanCopyMethodLong>();
+            
+        }
+    }
+}
