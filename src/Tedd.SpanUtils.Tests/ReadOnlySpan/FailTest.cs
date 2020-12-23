c1b197d7adb4edea5e60e7e9031461b3eeda3604
Tedd Hansen <tedd@konge.net>
2020-12-23 15:09:09 +0100

    Test xunit runner

---
+++ src/Tedd.SpanUtils.Tests/ReadOnlySpan/FailTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Tedd.SpanUtils.Tests.ReadOnlySpan
+{
+    public class FailTest
+    {
+        [Fact]
+        public void TestSByte()
+        {
+            Assert.True(false, "Should fail");
+        }
+    }
+}
