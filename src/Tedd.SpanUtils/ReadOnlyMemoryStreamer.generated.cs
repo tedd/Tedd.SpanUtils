be3e9efea924d24aa304dcbec5add23d1014ff13
Tedd Hansen <tedd@konge.net>
2021-01-19 21:47:26 +0100

    Generating more LE/BE

--- src/Tedd.SpanUtils/ReadOnlyMemoryStreamer.generated.cs
+++ src/Tedd.SpanUtils/ReadOnlyMemoryStreamer.generated.cs
@@ -22,7 +22,7 @@ namespace Tedd {
 
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override int ReadByte() => ReadByte(out _);
+        public Byte ReadByte() => ReadByte(out _);
 
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -417,6 +417,534 @@ namespace Tedd {
         public String ReadSizedString(out int length) {
             var ret = SpanUtils.ReadSizedString(Memory.Span.Slice(_position), out length);
             Position += length;
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public SByte ReadSByteLE() => ReadSByteLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public SByte ReadSByteLE(out int length) {
+            var ret = SpanUtils.ReadSByteLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(SByte);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Byte ReadByteLE() => ReadByteLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Byte ReadByteLE(out int length) {
+            var ret = SpanUtils.ReadByteLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Byte);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUInt16LE() => ReadUInt16LE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUInt16LE(out int length) {
+            var ret = SpanUtils.ReadUInt16LE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadInt16LE() => ReadInt16LE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadInt16LE(out int length) {
+            var ret = SpanUtils.ReadInt16LE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUInt32LE() => ReadUInt32LE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUInt32LE(out int length) {
+            var ret = SpanUtils.ReadUInt32LE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadInt32LE() => ReadInt32LE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadInt32LE(out int length) {
+            var ret = SpanUtils.ReadInt32LE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadUInt64LE() => ReadUInt64LE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadUInt64LE(out int length) {
+            var ret = SpanUtils.ReadUInt64LE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadInt64LE() => ReadInt64LE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadInt64LE(out int length) {
+            var ret = SpanUtils.ReadInt64LE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadSingleLE() => ReadSingleLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadSingleLE(out int length) {
+            var ret = SpanUtils.ReadSingleLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Single);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Double ReadDoubleLE() => ReadDoubleLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Double ReadDoubleLE(out int length) {
+            var ret = SpanUtils.ReadDoubleLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Double);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Decimal ReadDecimalLE() => ReadDecimalLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Decimal ReadDecimalLE(out int length) {
+            var ret = SpanUtils.ReadDecimalLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Decimal);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBooleanLE() => ReadBooleanLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBooleanLE(out int length) {
+            var ret = SpanUtils.ReadBooleanLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Boolean);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Char ReadCharLE() => ReadCharLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Char ReadCharLE(out int length) {
+            var ret = SpanUtils.ReadCharLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Char);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadFloatLE() => ReadFloatLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadFloatLE(out int length) {
+            var ret = SpanUtils.ReadFloatLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Single);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadShortLE() => ReadShortLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadShortLE(out int length) {
+            var ret = SpanUtils.ReadShortLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUShortLE() => ReadUShortLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUShortLE(out int length) {
+            var ret = SpanUtils.ReadUShortLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadIntLE() => ReadIntLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadIntLE(out int length) {
+            var ret = SpanUtils.ReadIntLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUIntLE() => ReadUIntLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUIntLE(out int length) {
+            var ret = SpanUtils.ReadUIntLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadLongLE() => ReadLongLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadLongLE(out int length) {
+            var ret = SpanUtils.ReadLongLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadULongLE() => ReadULongLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadULongLE(out int length) {
+            var ret = SpanUtils.ReadULongLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBoolLE() => ReadBoolLE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBoolLE(out int length) {
+            var ret = SpanUtils.ReadBoolLE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Boolean);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt24 ReadUInt24LE() => ReadUInt24LE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt24 ReadUInt24LE(out int length) {
+            var ret = SpanUtils.ReadUInt24LE(Memory.Span.Slice(_position), out length);
+            Position += 3;
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public SByte ReadSByteBE() => ReadSByteBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public SByte ReadSByteBE(out int length) {
+            var ret = SpanUtils.ReadSByteBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(SByte);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Byte ReadByteBE() => ReadByteBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Byte ReadByteBE(out int length) {
+            var ret = SpanUtils.ReadByteBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Byte);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUInt16BE() => ReadUInt16BE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUInt16BE(out int length) {
+            var ret = SpanUtils.ReadUInt16BE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadInt16BE() => ReadInt16BE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadInt16BE(out int length) {
+            var ret = SpanUtils.ReadInt16BE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUInt32BE() => ReadUInt32BE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUInt32BE(out int length) {
+            var ret = SpanUtils.ReadUInt32BE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadInt32BE() => ReadInt32BE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadInt32BE(out int length) {
+            var ret = SpanUtils.ReadInt32BE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadUInt64BE() => ReadUInt64BE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadUInt64BE(out int length) {
+            var ret = SpanUtils.ReadUInt64BE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadInt64BE() => ReadInt64BE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadInt64BE(out int length) {
+            var ret = SpanUtils.ReadInt64BE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadSingleBE() => ReadSingleBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadSingleBE(out int length) {
+            var ret = SpanUtils.ReadSingleBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Single);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Double ReadDoubleBE() => ReadDoubleBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Double ReadDoubleBE(out int length) {
+            var ret = SpanUtils.ReadDoubleBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Double);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Decimal ReadDecimalBE() => ReadDecimalBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Decimal ReadDecimalBE(out int length) {
+            var ret = SpanUtils.ReadDecimalBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Decimal);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBooleanBE() => ReadBooleanBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBooleanBE(out int length) {
+            var ret = SpanUtils.ReadBooleanBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Boolean);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Char ReadCharBE() => ReadCharBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Char ReadCharBE(out int length) {
+            var ret = SpanUtils.ReadCharBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Char);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadFloatBE() => ReadFloatBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Single ReadFloatBE(out int length) {
+            var ret = SpanUtils.ReadFloatBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Single);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadShortBE() => ReadShortBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int16 ReadShortBE(out int length) {
+            var ret = SpanUtils.ReadShortBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUShortBE() => ReadUShortBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt16 ReadUShortBE(out int length) {
+            var ret = SpanUtils.ReadUShortBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt16);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadIntBE() => ReadIntBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int32 ReadIntBE(out int length) {
+            var ret = SpanUtils.ReadIntBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUIntBE() => ReadUIntBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt32 ReadUIntBE(out int length) {
+            var ret = SpanUtils.ReadUIntBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt32);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadLongBE() => ReadLongBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Int64 ReadLongBE(out int length) {
+            var ret = SpanUtils.ReadLongBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Int64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadULongBE() => ReadULongBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt64 ReadULongBE(out int length) {
+            var ret = SpanUtils.ReadULongBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(UInt64);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBoolBE() => ReadBoolBE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Boolean ReadBoolBE(out int length) {
+            var ret = SpanUtils.ReadBoolBE(Memory.Span.Slice(_position), out length);
+            Position += sizeof(Boolean);
+            return ret;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt24 ReadUInt24BE() => ReadUInt24BE(out _);
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UInt24 ReadUInt24BE(out int length) {
+            var ret = SpanUtils.ReadUInt24BE(Memory.Span.Slice(_position), out length);
+            Position += 3;
             return ret;
         }
 
